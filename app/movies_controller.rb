d1ad048ef7d1d2d168b9360de3bcebfe3edc6ade
Karl Amoguis <ktamoguis@gmail.com>
2018-06-06 03:10:07 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -66,7 +66,7 @@ def can_find_using_where_clause_and_be_sorted
       movies << movie
     end
   end
-  binding.pry
+  sorted = movies.order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
